0b535b3ea4b61e86a34838c44133acae644c9c96
Димка я <  @mail.ru>
2022-09-30 21:18:23 +0300

    Task 19 palindrom chislo

---
+++ Task19/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+Console.WriteLine("введите пятизначное число: ");
+string array = Console.ReadLine();
+if (array[0]==array[4] && array[1] == array [3])
+Console.WriteLine("Yes");
+else
+Console.WriteLine("No");
